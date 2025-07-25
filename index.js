867c57d2187092f1bc952afda2dc7c51ebc6bebf
Priyanshu <youremail@example.com>
2025-07-25 13:51:04 +0530

    Solve cors error

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ import authRouter from "./routes/auth.routes.js";
 import cors from "cors"
 import userRouter from "./routes/user.routes.js";
 dotenv.config()
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://linkedin-frontend-rjyt.vercel.app",
+];
 const port=process.env.PORT
 let app=express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
